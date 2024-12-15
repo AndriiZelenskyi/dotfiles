94e4ab62cfea05d20701915fbd99eb5da4540e97
Andrii Zelenskyi <drew.zelensk1y@gmail.com>
2024-12-15 23:29:55 +0200

    feat: init tmux and color scheme

---
+++ setup.sh
@@ -0,0 +1,66 @@
+#! /bin/bash
+
+if ! [ -x "$(command -v brew)" ]; then
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if [[ ! -d /Applications/iTerm.app ]]; then
+    echo "Installing iTerm2..."
+    brew install --cask iterm2
+fi
+
+if ! [ -x "$(command -v git)" ]; then
+    echo "Installing Git..."
+    brew install git
+fi
+
+if ! [ -x "$(command -v zsh)" ]; then
+    echo "Installing Zsh..."
+    brew install zsh
+fi
+
+if ! [ -x "$(command -v tmux)" ]; then
+    echo "Installing Tmux..."
+    brew install tmux
+fi
+
+if ! [ -x "$(command -v nvim)" ]; then
+    echo "Installing Neovim..."
+    brew install neovim
+fi
+
+if ! [ -x "$(command -v fzf)" ]; then
+    echo "Installing Fzf..."
+    brew install fzf
+fi
+
+if ! [ -x "$(command -v rg)" ]; then
+    echo "Installing Ripgrep..."
+    brew install ripgrep
+fi
+
+echo "Copying configurations..."
+if [ -f ~/.tmux.conf ]; then
+    echo "Local tmux config already exists. Moving to tmux.bak"
+    mv -f ~/.tmux.conf ~/.tmux.conf.bak
+fi
+echo "Creating new tmux config..."
+if [[ ! -d ~/.config/tmux ]]; then
+    echo "Creating new tmux directory..."
+    mkdir ~/.config/tmux
+fi
+rm -rf ~/.config/tmux
+cp -r ./config/tmux ~/.config/tmux
+cp ./config/.tmux.conf ~/.tmux.conf
+tmux source ~/.tmux.conf
+
+# if [[ -d ~/.zshrc ]]; then
+#     echo "Local .zshrc already exists. Moving to .zshrc.bak"
+#     mv ~/.zshrc ~/.zshrc.bak
+#     rm ~/.zshrc
+# fi
+# ln ./config/.zshrc ~/.zshrc
+
+
+
